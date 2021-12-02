8db8306508fea6da8d6763b79eba3401f893cdb7
Richard Thiel <thielo@richard-thiel.de>
2021-12-02 01:37:08 +0100

    Initial Commit
    Co-authored-by: Richard Thiel <richard.thiel@real-digital.de>

---
+++ config/hashids.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright (c) Vincent Klaiber.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/vinkla/laravel-hashids
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hashids Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'salt' => env('SALT_MAIN', 'ud$y,|P/w 0>g]AGL&3eNZ9!?kcwmS(DvZa$18|6v97//[<x+P.)%0S )){#-7Xh'),
+            'length' => '5',
+            // 'alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        ],
+
+        'alternative' => [
+            'salt' => env('SALT_ALT', '0tYCXU^YcWzL*SG/n2>egJC`;+(cRQ_-o=1(Br(l^[fMt})}hr~33MUbE#ymaARP',),
+            'length' => '5',
+            // 'alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        ],
+
+    ],
+
+];
